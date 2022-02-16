0d5ae4c1a041a35bd9894149cf6cabddb663c97f
qxz1rhb <christoph-georg.schmied@capgemini.com>
2022-02-16 14:04:17 +0100

    Create Item Views 2

---
+++ WishList/Controllers/ItemController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WishList.Data;
+
+namespace WishList.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ItemController(ApplicationDbContext applicationDbContext)
+        {
+            _context = applicationDbContext;
+        }
+
+        public IActionResult Index() {
+            return View("Index");
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        
+        public IActionResult Delete()
+        {
+            return View();
+        }
+    }
+}
