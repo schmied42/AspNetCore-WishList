7b2fbc862d22eaa23423421ed0f9854a507f8cd6
qxz1rhb <christoph-georg.schmied@capgemini.com>
2022-02-16 15:06:33 +0100

    Create The ItemController 4

--- WishList/Controllers/ItemController.cs
+++ WishList/Controllers/ItemController.cs
@@ -33,10 +33,10 @@ namespace WishList.Controllers
             return RedirectToAction("Index");
         }
         
-        public IActionResult Delete(int Id)
+        public IActionResult Delete(int id)
         {
-            var tmpItem = _context.Items.FirstOrDefault(e => e.Id == Id);
-            _context.Items.Remove(tmpItem);
+            var item = _context.Items.FirstOrDefault(e => e.Id == id);
+            _context.Items.Remove(item);
             _context.SaveChanges();
             return RedirectToAction("Index");
         }
